68de1733fe1711ccd39c760482620fd2d5c66b53
kalaivanan564 <kalaivananeswaran564@gmail.com>
2025-07-03 14:37:24 +0530

    practice

---
+++ addarr.js
@@ -0,0 +1,27 @@
+let colors=["red","green","blue"]
+console.log(colors);
+
+colors.push("yellow"); // Add "yellow "to the end of the array
+console.log(colors);
+
+colors.unshift("purple"); // Add "purple"to the beginning of the array
+console.log(colors);
+
+colors.shift(); //Remove the first element of the array
+console.log(colors);
+
+colors[0]="orange"; //change the first element of the array
+console.log(colors);
+
+let Girls = ["Cecilie", "Lone"];
+let Boys = ["Emil", "Tobias", "Linus"];
+console.log(Girls);
+console.log(Boys);
+let children=Girls.concat(Boys); //combine two arrays(concatenation of arrays)
+console.log(children);
+
+
+
+
+
+
