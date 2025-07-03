3a06792fc9a36a32e40a0caa5a13989467307464
kalaivanan564 <kalaivananeswaran564@gmail.com>
2025-07-03 14:57:12 +0530

    modified

--- objectpra.js
+++ objectpra.js
@@ -1,1 +1,15 @@
 
+let car={"make":"Toyota","model":"corolla","year":2020,"color":"red"} //create an object
+console.log(car);
+
+let carmodel=car.model;
+console.log("The model of the car is:",carmodel);
+
+delete car.color;
+console.log("After deleting color property:",car);//delete the color property
+
+car.year=2022; //update the year
+console.log ("After updating year property:",car);
+
+
+
