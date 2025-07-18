c5c1adf0453b970f85fc015a2c3dcb96d1feee8f
kalaivanan564 <kalaivananeswaran564@gmail.com>
2025-07-18 11:33:17 +0530

    Events

---
+++ Events/data.js
@@ -0,0 +1,21 @@
+
+function display(){
+    document.getElementById("one").style.backgroundColor="lightblue"
+}
+
+function display1(){
+    document.getElementById("two").style.backgroundColor="lightblue"
+}
+function Onclick(){
+    document.getElementById("three").style.backgroundColor="lightblue"
+}
+function Ondbclick(){
+    document.getElementById("four").style.backgroundColor="lightblue"
+}
+function changeBgcolor(){
+    document.getElementById("five").style.backgroundColor="lightblue"
+}
+function toUpperCase(){
+    let input=document.getElementById("six")
+    input.value=input.value.toUpperCase()
+}
