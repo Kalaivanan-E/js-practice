159ab09ea2c44b531ccff9b31e32323bc0c1272c
kalaivanan564 <kalaivananeswaran564@gmail.com>
2025-09-05 11:55:04 +0530

    lifecyclecomponent

--- react31/react31/src/App.js
+++ react31/react31/src/App.js
@@ -1,37 +1,20 @@
-import { BrowserRouter, Link, Route, Routes } from "react-router"
-import Home from "./Components/Home"
-import About from "./Components/About"
-import Service from "./Components/Service"
-import Contact from "./Components/Contact"
-import { NavLink } from "react-router"
-import './App.css'
 
-let App = ()=>{
-  let mystyles = {textAlign:"center", marginTop:"300px", color:"blue", fontSize:"50px"}
+import {NavLink,BrowserRouter as Router,Routes,Route} from 'react-router'
+import Navbar from './Navbar';
+import Home from './components/Home'
 
-  return <>
-    <BrowserRouter>
-      <nav  className="navbar navbar-dark bg-dark navbar-expand-lg" id="navbar">
-      {/* <NavLink to = "/index" id="nav" className="navbar-brand ">Logo</NavLink> */}
-     <NavLink to='/index'> <img id="img" src="https://static.vecteezy.com/system/resources/previews/011/853/549/original/technology-logo-design-template-free-vector.jpg" alt="image" /> </NavLink>
-      <div className="ml-auto" id="menu">
-        <ul className="navbar-nav">
-          <li><NavLink to = '/index' className="nav-link  color-dark">Home</NavLink></li>
-          <li><NavLink to = '/about' className="nav-link">About</NavLink></li>
-          <li><NavLink to = '/service' className="nav-link">Service</NavLink></li>
-          <li><NavLink to = '/contact' className="nav-link">Contact</NavLink></li>
-        </ul>
-      </div>
-    </nav>
-    <Routes>
-      <Route path="/index" element = {<Home/>}/>
-      <Route path="/about" element = {<About/>}/>
-      <Route path="/service" element = {<Service/>}/>
-      <Route path="/contact" element = {<Contact/>}/>
-      <Route path="*" element = {<h2 style={mystyles}>404 -Page Not Found</h2>}/>
-    </Routes>
-    </BrowserRouter>
-  </>
+let App=()=>{
+    let mystyles={color:"blue"}
+    return <div>
+            <Router>
+                <Navbar/>
+                <Routes>
+                    <Route path="/"  element={<Home/>}/>
+                    <Route path="/index"  element={<Home/>}/>
+                   
+                    <Route path="*" element={<h3 style={mystyles}>404 - Page Not Found</h3>} />
+                </Routes>
+            </Router>
+        </div>
 }
-
-export default App
+export default App;
---
+++ react31/react31/src/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {NavLink} from 'react-router'
+const Navbar = () => {
+  return <nav className='navbar navbar-dark bg-dark navbar-expand-md'>
+                    <NavLink to="/index" className='navbar-brand'>Logo</NavLink>
+                    <div className='ml-auto'>
+                        <ul className='navbar-nav'>
+                            <li><NavLink to="/index" className='nav-link'>Home</NavLink></li>
+                            <li><NavLink to="/employees" className='nav-link'>Employees</NavLink></li>
+                            <li><NavLink to="/login" className='nav-link'>Login</NavLink></li>  
+                        </ul>
+                    </div>
+                </nav>
+}
+
+export default Navbar
