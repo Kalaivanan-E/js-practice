6d59da02a70dd5b3edcde808bab67bb12b5d4047
kalaivanan564 <kalaivananeswaran564@gmail.com>
2025-09-30 14:42:02 +0530

    project

--- Project/responsive websites/edusity/src/App.jsx
+++ Project/responsive websites/edusity/src/App.jsx
@@ -3,14 +3,24 @@ import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Navbar/Hero/Hero'
 import Programs from './Components/Programs/Programs'
 import Title from './Components/Title/Title'
+import About from './Components/About/About'
+import Campus from './Components/Campus/Campus'
+import Contact from './Components/Contact/Contact'
+import Footer from './Components/Footer/Footer'
 
 const App = () => {
   return (
     <div>
       <Navbar/>
       <Hero/>
-      <Title/>
+      <Title subTitle = 'Our PROGRAM' title = 'What We Offer'/>
       <Programs/>
+      <About/>
+      <Title subTitle = 'Gallery' title = 'Campus Photos'/>
+      <Campus/>
+      <Title subTitle = 'Contact Us' title = 'Get in Touch'/>
+      <Contact/>
+      <Footer/>
       
     </div>
   )
