06eaccea549be440c82e1cda570ee81c76627385
kalaivanan564 <kalaivananeswaran564@gmail.com>
2025-10-13 11:08:59 +0530

    JS -2

---
+++ JAVASCRIPT BATCH2/Class 1/Test.js
@@ -0,0 +1,15 @@
+
+
+console.log("Hello World")
+document.writeln("Hello world")
+
+
+var eid = 101;
+var ename = 'Rahul'
+var esal = 45000.45
+var avail = false
+
+document.writeln(eid)
+document.writeln(ename)
+document.writeln(esal)
+document.writeln(avail)
---
+++ js1/Arrary/Array.js
@@ -0,0 +1,11 @@
+
+// Array 
+
+let veges = ['carrot',{name:'goms',age:23}, 'beetroot','pumkin',true,null,undefined,23]
+
+console.log(typeof(veges))  // object
+console.log(veges) // ['carrot', 'beetroot']
+
+console.log(veges[0 ]) // carrot
+console.log(veges[6]) // 23
+console.log(veges[1]) // 
