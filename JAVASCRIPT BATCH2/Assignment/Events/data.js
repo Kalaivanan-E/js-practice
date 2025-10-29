223bb23e6981a12e2d7ade4951dabcbd94e34f11
kalaivanan564 <kalaivananeswaran564@gmail.com>
2025-10-29 19:23:37 +0530

    js assignment

---
+++ JAVASCRIPT BATCH2/Assignment/Events/data.js
@@ -0,0 +1,35 @@
+
+function display1(){
+    document.getElementById('one').style.backgroundColor = "lightblue"
+    document.getElementById('onmouseover').innerText="Mouse over to change button background color"
+}
+
+function display2(){
+    document.getElementById('two').style.backgroundColor = "lightblue"
+    document.getElementById('onmouseout').innerText="Mouse over to change button background color"
+} 
+function display3(){
+    document.getElementById("three").style.backgroundColor = "lightblue"
+    document.getElementById("onclick").innerText = "Click to change button background color"
+}
+function display4(){
+    document.getElementById("four").style.backgroundColor = "lightblue"
+    document.getElementById("ondblclick").innerText = "Double Click to change button background color"
+}
+function display5(){
+    document.getElementById("five").style.backgroundColor = "lightblue"
+    document.getElementById("Onfocus").innerText = "Click to change input background color"
+}
+function display6(){
+    let input = document.getElementById("six")
+     input.value = input.value.toLowerCase()
+    
+}
+
+function password(){
+    let enteredtext= document.getElementById("login").value
+    let pass = "password"
+   if(enteredtext == pass){
+    alert('login succues')
+   }
+}
