32b6b5da75b0958d4508bb0af1448a1428f3bd20
kalaivanan564 <kalaivananeswaran564@gmail.com>
2025-11-05 22:26:56 +0530

    js

---
+++ javascript/Array New methods/forEach/two copy.js
@@ -0,0 +1,11 @@
+// map 
+
+let prices = [98,198,298,398,498]
+// create new prices with add+1
+// exprected output [99,199,299,399,499]
+
+let new_prices = prices.map((prices)=>{
+    return prices = prices +1
+})
+
+console.log(new_prices)
