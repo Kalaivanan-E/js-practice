dd026109df6d17ad9bc3e5697400c49cb2c2c731
kalaivanan564 <kalaivananeswaran564@gmail.com>
2025-11-06 14:50:37 +0530

    fetch API

--- JAVASCRIPT BATCH2/Assignment/fetch API/data.js
+++ JAVASCRIPT BATCH2/Assignment/fetch API/data.js
@@ -2,11 +2,11 @@
 
 let display = ()=>{
 
-    console.log("good morning")
+    // console.log("good morning")
     fetch("https://jsonplaceholder.typicode.com/users")
     .then((response) => response.json())
     .then((data)=>{
-        console.log("data fetching successfully")
+        // console.log("data fetching successfully")
         let tableBody = document.getElementById('one')
         let row = '';
         data.forEach((user)=>{
