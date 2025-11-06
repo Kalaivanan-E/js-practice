1c8d89a236b35c8290c298486802118de105cb45
kalaivanan564 <kalaivananeswaran564@gmail.com>
2025-11-06 16:22:32 +0530

    js

--- JAVASCRIPT BATCH2/Assignment/fetch API/data.js
+++ JAVASCRIPT BATCH2/Assignment/fetch API/data.js
@@ -7,20 +7,19 @@ let display = ()=>{
     .then((response) => response.json())
     .then((data)=>{
         // console.log("data fetching successfully")
-        let tableBody = document.getElementById('one')
         let row = '';
         data.forEach((user)=>{
             row = row +  `
-          <tr>
-            <td>${user.id}</td>
-            <td>${user.name}</td>
-            <td>${user.email}</td>
-            <td>${user.address.city}</td>
-          </tr> `;
+                          <tr>
+                            <td>${user.id}</td>
+                            <td>${user.name}</td>
+                            <td>${user.email}</td>
+                            <td>${user.address.city}</td>
+                          </tr> `;
         
       });
 
-        tableBody.innerHTML = row
+      document.getElementById('one').innerHTML = row
 
     })
     .catch((err)=>{console.log("error fetching data:",err)})
