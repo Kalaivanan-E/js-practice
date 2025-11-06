511de2e8ef73069270b79b8cb244b42b432e3224
kalaivanan564 <kalaivananeswaran564@gmail.com>
2025-11-06 12:23:07 +0530

    fetch api and convert into HTML table format

---
+++ JAVASCRIPT BATCH2/Assignment/fetch API/data.js
@@ -0,0 +1,29 @@
+
+
+let display = ()=>{
+
+    console.log("good morning")
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then((response) => response.json())
+    .then((data)=>{
+        console.log("data fetching successfully")
+        let tableBody = document.getElementById('one')
+        let row = '';
+        data.forEach((user)=>{
+            row = row +  `
+          <tr>
+            <td>${user.id}</td>
+            <td>${user.name}</td>
+            <td>${user.email}</td>
+            <td>${user.address.city}</td>
+          </tr> `;
+        
+      });
+
+        tableBody.innerHTML = row
+
+    })
+    .catch((err)=>{console.log("error fetching data:",err)})
+}
+
+ 
