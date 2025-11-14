fbf8894215e6d98532d9563ab9df686c2117bb43
kalaivanan564 <kalaivananeswaran564@gmail.com>
2025-11-14 11:15:19 +0530

    async & await

---
+++ JAVASCRIPT BATCH2/Class 20/Sync_Async/two.js
@@ -0,0 +1,14 @@
+// asynchronous
+
+setTimeout(() => {
+    console.log("Request 1")
+}, 5000);
+setTimeout(() => {
+    console.log("Request 2")
+}, 1000);
+setTimeout(() => {
+    console.log("Request 3")
+}, 4000);
+setTimeout(() => {
+    console.log("Request 4")
+}, 3000);
