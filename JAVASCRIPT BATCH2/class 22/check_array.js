6ac4442f0c62027ebb8a799e714f280cf5995aa3
kalaivanan564 <kalaivananeswaran564@gmail.com>
2025-11-18 10:50:35 +0530

    array is empty or not

---
+++ JAVASCRIPT BATCH2/class 22/check_array.js
@@ -0,0 +1,9 @@
+
+// verify the given array is empty or not?
+
+let nums = [];
+let user = [{},{}];
+user.length>0? console.log("The given array is not empty"): console.log('The given array is empty'); // not empty
+nums.length>0? console.log("The given array is not empty"): console.log('The given array is empty'); // empty
+
+
