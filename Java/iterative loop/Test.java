1ee9c1c9181c168e2fa7ae4851d7347cc99679f0
kalaivanan564 <kalaivananeswaran564@gmail.com>
2025-11-18 22:34:38 +0530

    assignment

---
+++ Java/iterative loop/Test.java
@@ -0,0 +1,18 @@
+
+// write a progrom to print 1 to 15 using for loop?
+
+/* class Test{
+    public static void main(String[] args) {
+        for (int i =1; i<=15; i++){
+            System.out.println(i);
+        }
+    }
+} */
+// write a program to print 7 table using for loop
+class Test{
+    public static void main(String[] args) {
+        for (int i = 1; i<=10; i++){
+            System.out.println("7 *" + i + "="+ ( 7*i));
+        }
+    }
+}
---
+++ Java/iterative loop/Testone.java
@@ -0,0 +1,39 @@
+class Testone{
+    // write a program to check if a number is divisible by 7 or not
+    public void m1(){
+        int num1 = 8;
+        if(num1 % 7 == 0){
+            System.out.println("The given number is divisible by 7");
+        }
+        else{
+            System.out.println("The given number is not divisible by 7");
+        }
+    }
+    // write a program to check if a number is divisible by 3 or not
+    public void m2(){
+        int num2 = 4;
+        if(num2 % 3 == 0){
+            System.out.println("The given number is divisible by 3");
+        }
+        else{
+            System.out.println("The given number is not divisible by 3");
+        }
+    }
+    // write a program to check the given number is positive or negative
+    public void m3(){
+        int num3 = -1;
+        if(num3 >= 0){
+            System.out.println("The given number is positive number");
+        }
+        else{
+            System.out.println("The given number is negative number");
+        }
+    }
+    public static void main(String[] args) {
+        Testone a = new Testone();
+        a.m1();
+        a.m2();
+        a.m3();
+    }
+
+}
