995a5064f35e4e838cc00e041ebbb510390316ef
kalaivanan564 <kalaivananeswaran564@gmail.com>
2025-11-26 13:10:16 +0530

    react js

--- JAVASCRIPT BATCH2/Assignment/Test/Test.java
+++ JAVASCRIPT BATCH2/Assignment/Test/Test.java
@@ -5,6 +5,7 @@ class Test{
         int n = scan.nextInt();
         for (int i=1; i<=n; i++){
             for(int j=1; j<=n; j++){
+                // System.out.print( " *");
                 if(i == 1 || j == n || j ==1 || i ==n){
                     System.out.print( "* ");
                 }
@@ -12,7 +13,8 @@ class Test{
                     System.out.print("  ");
                 }
             }
-            System.out.println();
+            System.out.println( );
         }
     }
 }
+
