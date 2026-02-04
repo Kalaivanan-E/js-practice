ec2324c41b45a3fc8a1b8d53c4c881f9f33daf4c
kalaivanan564 <kalaivananeswaran564@gmail.com>
2026-02-04 17:02:09 +0530

    java

--- Technical rounds/Java/RemoveDuplicatess/Test1.java
+++ Technical rounds/Java/RemoveDuplicatess/Test1.java
@@ -1,6 +1,8 @@
 import java.util.HashSet;
 import java.util.Set;
 
+
+// approach 1
 class removeDuplicates{
     public static void main(String[] args) {
         int arr[] = {1,2,3,4,1,2,5,2,1};
@@ -11,5 +13,24 @@ class removeDuplicates{
         }
         System.out.println("After removing duplicates:"+set);
         System.out.println(set.size());
+    }
+}
+//approach 2
+
+class removeDuplicates1{
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,1,2,5,2,1};
+        for(int i =0; i<arr.length; i++){
+            boolean isDuplicates = false;
+            for(int j=0; j<i; j++){
+                if(arr[i] == arr[j]){
+                    isDuplicates = true;
+                    break;
+                }
+            }
+            if(!isDuplicates){
+                System.err.print(arr[i] + " ");
+            }
+        }
     }
 }
