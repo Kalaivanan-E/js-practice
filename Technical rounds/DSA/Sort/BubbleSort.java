66ad7767eddf07ed5b0277fc14fc3252490a9614
kalaivanan564 <kalaivananeswaran564@gmail.com>
2026-02-10 16:24:51 +0530

    DSA bubble sort

---
+++ Technical rounds/DSA/Sort/BubbleSort.java
@@ -0,0 +1,33 @@
+
+
+// bubble sort is a sorting algorithm that compares adjacent elements and swaps them repeatedly untill the list is sorted.
+
+import java.util.*;
+class BubbleSort{
+
+    public static void sort(int[] arr){
+        int n = arr.length;
+        for(int i=0; i<n-1; i++){
+            boolean swap = false;
+            for(int j=0; j< n-i-1; j++){
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                    swap = true;
+                }
+            }
+            if(swap == false){
+                break;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int arr[] = {5,2,10,4,0};
+        int brr[] = {1,2,3,4,5};
+        sort(brr);  
+        sort(arr);
+        System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(brr));
+    }
+}
